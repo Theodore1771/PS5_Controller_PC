a26224f21c255aa70be1e54e615579ff2062395e
bashcashew <121883371+bashcashew@users.noreply.github.com>
2024-11-30 22:54:17 +0100

    Create PS5_Controller_PC.py

---
+++ PS5_Controller_PC.py
@@ -0,0 +1,21 @@
+#############################################################################
+#############################################################################
+##                                                                         ##
+##                                                                         ##
+## Title:            PS5-to-PC Controller and Re-mapper                    ##
+## Description:      This should allow for your PC to intercept the        ##
+##                   bluetooth signal from your PS5 controller. This will  ##
+##                   then be remapped to your own liking.                  ##
+## Features:         - Custom rumble per key press                         ##
+##                   - Re-mappable button, trigger presses                 ##
+##                   - Telemetric data in .csv (stored in a sub-folder)    ##
+##                   - mouse movement, single-click, and scrolling         ##
+## Version:          1.0.0                                                 ##
+##                                                                         ##
+##                                                                         ##
+#############################################################################
+#############################################################################
+
+
+
+
